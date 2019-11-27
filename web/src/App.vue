021ce904348562e3bc7526c3abb1f6ef383717bf
ggpereira <ggpereira@inf.ufsm.br>
2019-11-27 11:30:28 -0300

    frontend

---
+++ web/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo-white.png"
+            alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </a>
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+    <div class="container is-fluid">
+      <Table></Table>
+    </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Criado</strong> por
+          <a href="https://github.com/ggpereira">Gabriel Gomes Pereira</a>, 
+          <a href="https://github.com/odcmello">Otávio da Cruz Mello</a> and 
+          <a href="https://github.com/jcamaral">Juliano Colling</a>, 
+          Sistemas de Informação, Universidade Federal de Santa Maria, 2019
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Table from "./components/Table";
+
+export default {
+  name: "app",
+  components: {
+    Table
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/components/Table.vue
@@ -0,0 +1,70 @@
+<template>
+  <section>
+    <b-table :data="data" :columns="columns"> </b-table>
+  </section>
+</template>
+
+nome;job_title;company;location;url;curso;ano_evasao
+
+<script>
+    export default {
+        name: "Table",
+        data() {
+            return {
+                 data: [
+                    { 'id': 1, 'nome': 'Jesse', 'job_title': 'Simmons', 'company': '2016-10-15 13:43:27', 'location': 'Male', 'url':'https://www.google.com', 'curso':'Sistemas de Informação', 'ano_evasao': 2017 },
+                    { 'id': 2, 'nome': 'John', 'job_title': 'Simmons', 'company': '2016-10-15 13:43:27', 'location': 'Male', 'url':'https://www.google.com', 'curso':'Sistemas de Informação', 'ano_evasao': 2017 },
+                    { 'id': 3, 'nome': 'Tina', 'job_title': 'Simmons', 'company': '2016-10-15 13:43:27', 'location': 'Male', 'url':'https://www.google.com', 'curso':'Sistemas de Informação', 'ano_evasao': 2017 },
+                    { 'id': 4, 'nome': 'Clarence','job_title': 'Simmons', 'company': '2016-10-15 13:43:27', 'location': 'Male', 'url':'https://www.google.com', 'curso':'Sistemas de Informação', 'ano_evasao': 2017 },
+                    { 'id': 5, 'nome': 'Anne', 'job_title': 'Simmons', 'company': '2016-10-15 13:43:27', 'location': 'Male', 'url':'https://www.google.com', 'curso':'Sistemas de Informação', 'ano_evasao': 2017  }
+                ],
+                columns: [
+                    {
+                        field: 'id',
+                        label: 'ID',
+                        width: '100',
+                        numeric: true,
+                        searchable: true,
+                    },
+                    {
+                        field: 'nome',
+                        label: 'Nome',
+                        searchable: true,
+                    },
+                    {
+                        field: 'job_title',
+                        label: 'Cargo',
+                        searchable: true,
+                    },
+                    {
+                        field: 'company',
+                        label: 'empresa',
+                        searchable: true,
+                    },
+                    {
+                        field: 'location',
+                        label: 'Localização',
+                        centered: true,
+                        searchable: true,
+                    },
+                    {
+                        field: 'url',
+                        label: 'Linkedin',
+                    }, 
+                    {
+                        field: 'curso',
+                        label: 'Curso',
+                        searchable: true,
+                    }, 
+                    {
+                        field: 'ano_evasao', 
+                        label: 'Ano',
+                        searchable: true,
+                        width: 100
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
